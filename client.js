f3fc222056786f8d87b198ac1680fbd2d91de178
Adrian Niu <niuxx107@umn.edu>
2020-03-03 16:08:06 -0600

    completted the bonus Calculator

--- client.js
+++ client.js
@@ -39,5 +39,61 @@ const employees = [
 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
+$(document).ready(function() {
+  $( '#button' ).click( calculateEmployees );
+});
 
-console.log( employees );
+function bonusCalculator(employee){
+  let name = employee.name;
+  let bonusPercentage = 0;
+  let totalCompensation = 0;
+  let totalBonus = 0;
+
+  //Checking the rating
+  if (employee.reviewRating === 3){
+    bonusPercentage = 0.04;
+  }else if (employee.reviewRating === 4){
+    bonusPercentage = 0.06;
+  } else if (employee.reviewRating === 5){
+    bonusPercentage = 0.10;
+  }
+
+  //Checking the employee number length
+  if (employee.employeeNumber.length === 4){
+    bonusPercentage += 0.05;
+  }
+
+  //Checking the employee income too much?
+  if (employee.annualSalary > 65000){
+    bonusPercentage -= 0.01;
+  }
+  
+  //Checking if the employee bonus exceed normal range
+  if (bonusPercentage > 0.13 ){
+    bonusPercentage = 0.13;
+  } 
+  else if(bonusPercentage < 0){
+    bonusPercentage = 0;
+  }
+
+  totalBonus = Math.round(Number(employee.annualSalary) * bonusPercentage);
+  totalCompensation += Number(employee.annualSalary) + totalBonus;
+
+  employeeBonuses = {
+    name: name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus
+  };
+  $('ul').append(`<li>Emplayee Name: ${employeeBonuses.name} --- Bonus %: ${employeeBonuses.bonusPercentage} --- Compensation: ${employeeBonuses.totalCompensation} --- Bonus: ${employeeBonuses.totalBonus}</li>`)
+  return employeeBonuses
+  
+}
+
+function calculateEmployees(){
+  $('ul').text('');
+  for (let employee of employees) {
+    console.log(bonusCalculator(employee));
+  }
+}
+
